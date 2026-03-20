4f028b0ed2f46ff8b9870cab519533ce8b8c6b86
rosarioborgesgt-hkmatata <rosario.borges.gt@gmail.com>
2026-03-20 20:39:38 +0100

    Mover clases al paquete tarea4

--- JavaApplication9/src/tarea4/JavaApplication9.java
+++ JavaApplication9/src/tarea4/JavaApplication9.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication9;
+package tarea4;
 
 public class JavaApplication9 {
 
     public static void main(String[] args) {
-        System.out.println("Febrero de 2026 tiene " + Pruebas.diasEnMesArray(2, 2026) + " días");
+        System.out.println("Febrero de 2026 tiene " + Pruebas.diasEnMesArray(2, 2026) + " dï¿½as");
         System.out.println("60mph son " + Pruebas.kmhAms(Pruebas.mphAKmh(60)) + " metros por segundo");
         System.out.println("Si el cateto A mide 3cm y el cateto B mide 4cm, entonces la hipotenusa mide "
                 + Pruebas.hip(3, 4) + "cm");
