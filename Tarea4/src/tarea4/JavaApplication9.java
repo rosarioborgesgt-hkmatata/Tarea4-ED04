dd319103d8e699158cafc24a4404142ccedb1435
rosarioborgesgt-hkmatata <rosario.borges.gt@gmail.com>
2026-03-20 21:07:40 +0100

    Renombrar clases Pruebas y JavaApplication9

--- Tarea4/src/tarea4/JavaApplication9.java
+++ Tarea4/src/tarea4/JavaApplication9.java
@@ -7,10 +7,10 @@ package tarea4;
 public class JavaApplication9 {
 
     public static void main(String[] args) {
-        System.out.println("Febrero de 2026 tiene " + Pruebas.diasEnMesArray(2, 2026) + " d�as");
-        System.out.println("60mph son " + Pruebas.kmhAms(Pruebas.mphAKmh(60)) + " metros por segundo");
+        System.out.println("Febrero de 2026 tiene " + Utilidades.diasEnMesArray(2, 2026) + " d�as");
+        System.out.println("60mph son " + Utilidades.kmhAms(Utilidades.mphAKmh(60)) + " metros por segundo");
         System.out.println("Si el cateto A mide 3cm y el cateto B mide 4cm, entonces la hipotenusa mide "
-                + Pruebas.hipotenusa(3, 4) + "cm");
+                + Utilidades.hipotenusa(3, 4) + "cm");
     }
 
 }
